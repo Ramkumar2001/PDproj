9eb59ce6006db3bc3478389ae309c1c35462130a
1192923 <bharatmoorthykrishnamoorthy@gmail.com>
2024-01-28 20:35:46 +0530

    changed time in timer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var currentDate = new Date();
-    var activationDate = new Date('2024-01-26T00:00:00'); // Set the activation date and time
+    var activationDate = new Date('2024-02-18T00:00:00'); // Set the activation date and time
   
     if (currentDate >= activationDate) {
       document.getElementById('letter2').style.display = 'block';
